4075344eebdc1a7fbfc6fecd7f7d82f8076c1fdd
yuvraj <yuvraj.167@gmail.com>
2015-08-11 07:35:05 +0530

    First Commit

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,33 @@
+class ArticlesController < ApplicationController
+	
+
+	def index
+		@articles = Article.all
+	end
+
+	def new
+		@article =  Article.new
+	end
+	
+	def create
+		#render plain: params[:article].inspect
+		@article = Article.new(article_params)
+			if @article.save
+				redirect_to @article
+			else
+				render 'new'
+			end
+
+	end
+
+	def show
+		@article = Article.find(params[:id])
+	end
+
+
+    private
+	def article_params
+		params.require(:article).permit(:title, :text)
+	end
+
+end
